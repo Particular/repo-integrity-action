9d55d461828f289023aa7e1824d339f67fadde30
David Boike <david.boike@gmail.com>
2024-03-13 16:54:49 -0500

    Global suppressions in .editorconfig files

---
+++ src/RepoIntegrityTests/CodeFileTests.cs
@@ -0,0 +1,16 @@
+﻿namespace RepoIntegrityTests
+{
+    using NUnit.Framework;
+    using RepoIntegrityTests.Infrastructure;
+
+    [DotNetProjects]
+    public class CodeFileTests
+    {
+        [Test]
+        public void GlobalSuppressionsInEditorConfig()
+        {
+            new TestRunner("GlobalSuppressions.cs", "Global suppressions should be expressed in .editorconfig files so they are easily findable", failIfNoMatches: false)
+                .Run(f => f.Fail());
+        }
+    }
+}
